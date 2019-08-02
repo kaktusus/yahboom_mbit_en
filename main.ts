cd073f63ad399e25ef8be460ec0b753c0f396edd
kaktus Jacek <39754351+kaktusus@users.noreply.github.com>
2019-08-02 23:53:23 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -73,7 +73,7 @@ namespace mbit_Display {
         ON = 0
     }
 
-    //% blockId=mbit_LED_L8 block="LED_L8| %value"
+    //% blockId=mbit_LED_L8 block="LED_L8|value %value"
     //% weight=5
     //% blockGap=8
     //% color="#26cfed"
