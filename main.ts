93f29c5cbfe8cf2543aabb706b9433dd3bb1094b
kaktus Jacek <39754351+kaktusus@users.noreply.github.com>
2019-08-02 23:30:38 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -74,7 +74,7 @@ namespace mbit_Display {
     }
 
     //% blockId=mbit_LED_L8 block="LED_L8|value %value"
-    //% weight=3
+    //% weight=5
     //% blockGap=8
     //% color="#26cfed"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
