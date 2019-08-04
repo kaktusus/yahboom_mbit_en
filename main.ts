612fce1835be61efc61951deb4dd5833c673632a
kaktus Jacek <39754351+kaktusus@users.noreply.github.com>
2019-08-04 16:56:15 +0200

    Update main.ts
    ...

--- main.ts
+++ main.ts
@@ -60,6 +60,7 @@ namespace mbit_Display {
     //% block="LED|connected to %pin|state on %value"
     //% blockGap=8
     //% color="#C814B8"
+    //% weight=36
     //% pin.fieldEditor="gridpicker"
     //% pin.fieldOptions.width=220
     //% pin.fieldOptions.columns=3
@@ -79,6 +80,7 @@ namespace mbit_Display {
     //% block="LED|connected to %pin|state on %value"
     //% blockGap=8
     //% color="#C814B8"
+    //% weight=35
     //% value.min=0 value.max=255
     //% pin.fieldEditor="gridpicker"
     //% pin.fieldOptions.width=220
@@ -96,6 +98,7 @@ namespace mbit_Display {
     //% block="Breath LED|connected to %pin"
     //% blockGap=8
     //% color="#C814B8"
+    //% weight=34
     //% pin.fieldEditor="gridpicker"
     //% pin.fieldOptions.width=220
     //% pin.fieldOptions.columns=3
@@ -122,9 +125,10 @@ namespace mbit_Display {
 
 
 
-    //% blockId=mbit_LED_L8 block="blue LED_L8| state on %value"
+    //% blockId=mbit_LED_L8 block="blue LED_L8|state on %value"
     //% blockGap=8
     //% color="#932bb5"
+    //% weight=33
     //% valuePWM.fieldEditor="gridpicker"
     //% valuePWM.fieldOptions.width=260
     //% valuePWM.fieldOptions.columns=1
@@ -138,6 +142,7 @@ namespace mbit_Display {
     //% blockId=mbit_LED_L9 block="blue LED_L9| %value"
     //% blockGap=8
     //% color="#932bb5"
+    //% weight=32
     //% valuePWM.fieldEditor="gridpicker"
     //% valuePWM.fieldOptions.width=260
     //% valuePWM.fieldOptions.columns=1
@@ -157,6 +162,7 @@ namespace mbit_Display {
     //% block="RGB|Pin 1 Red connected to %pin1|Pin 2 Green connected to %pin2|Pin 3 connected to Blue %pin3|value Red %value1|value Green %value2|value Blue %value3"
     //% blockGap=8
     //% color="#C814B8"
+    //% weight=31
     //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
     //% pin1.fieldEditor="gridpicker" pin2.fieldEditor="gridpicker" pin3.fieldEditor="gridpicker"
     //% pin1.fieldOptions.width=220 pin2.fieldOptions.width=220 pin3.fieldOptions.width=220
@@ -176,6 +182,7 @@ namespace mbit_Display {
     //% block="RGB Led Ø10mm|Pin 1 Red connected to %pin1|Pin 2 Green connected to %pin2|Pin 3 Blue connected to %pin3|Set the colour %value"
     //% blockGap=8
     //% color="#C814B8"
+    //% weight=30
     //% pin1.fieldEditor="gridpicker" pin2.fieldEditor="gridpicker" pin3.fieldEditor="gridpicker"
     //% pin1.fieldOptions.width=200 pin2.fieldOptions.width=200 pin3.fieldOptions.width=200
     //% pin1.fieldOptions.columns=3 pin2.fieldOptions.columns=3 pin3.fieldOptions.columns=3
