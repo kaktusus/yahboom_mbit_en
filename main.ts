8758bd8047fa6b258949f6d8d8e30bc49f7ee67d
kaktus Jacek <39754351+kaktusus@users.noreply.github.com>
2019-08-04 15:54:54 +0200

    Update main.ts
    ...

--- main.ts
+++ main.ts
@@ -49,7 +49,6 @@ namespace mbit_Display {
 
     //% blockId=mbit_LED1
     //% block="LED|connected to %pin|state on %value"
-    //% weight=20
     //% blockGap=8
     //% color="#C814B8"
     //% pin.fieldEditor="gridpicker"
@@ -69,7 +68,6 @@ namespace mbit_Display {
 
     //% blockId=mbit_LED2
     //% block="LED|connected to %pin|state on %value"
-    //% weight=19
     //% blockGap=8
     //% color="#C814B8"
     //% value.min=0 value.max=255
@@ -87,7 +85,6 @@ namespace mbit_Display {
 
     //% blockId=mbit_BreathLED
     //% block="Breath LED|connected to %pin"
-    //% weight=18
     //% blockGap=8
     //% color="#C814B8"
     //% pin.fieldEditor="gridpicker"
@@ -122,7 +119,6 @@ namespace mbit_Display {
 
     //% blockId=mbit_RGB
     //% block="RGB|Pin 1 Red connected to %pin1|Pin 2 Green connected to %pin2|Pin 3 connected to Blue %pin3|value Red %value1|value Green %value2|value Blue %value3"
-    //% weight=17
     //% blockGap=8
     //% color="#C814B8"
     //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
@@ -142,7 +138,6 @@ namespace mbit_Display {
 
     //% blockId=mbit_RGB2
     //% block="RGB Led Ø10mm|Pin 1 Red connected to %pin1|Pin 2 Green connected to %pin2|Pin 3 Blue connected to %pin3|Set the colour %value"
-    //% weight=16
     //% blockGap=8
     //% color="#C814B8"
     //% pin1.fieldEditor="gridpicker" pin2.fieldEditor="gridpicker" pin3.fieldEditor="gridpicker"
